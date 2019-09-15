2409b168c436d83f83b223fc1b7b7461a64937cf
Sim4n6 <sim4n6@gmail.com>
2019-09-15 15:56:58 +0100

    added support for errors and book details functionnality

--- application.py
+++ application.py
@@ -34,3 +34,19 @@ def search():
         data["title"] = request.form['title']
         data["author"] = request.form['author']
     return render_template("search_form.html", data=data)
+
+
+@app.route("/book", methods=["GET"])
+@app.route("/book/<int:bid>", methods=["GET"])
+def book(bid=None):
+    if bid is None: 
+        return render_template("error.html", message="Please, provide a book id.")
+        
+    book_w_author = db.execute("SELECT * FROM books JOIN authors ON books.author_id = authors.id WHERE books.id = :bid ", {"bid":bid}).fetchone()
+    if book_w_author is None:
+        return render_template("error.html", message="The book id is not correct.")
+
+    return render_template("book.html", book = book_w_author)
+
+if __name__ == "__main__":
+    app.run(load_dotenv=True)
